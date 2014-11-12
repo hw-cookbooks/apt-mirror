c6bade75ef98ac38c5e431459e9bc58434e919c3
Graham Weldon <graham.weldon@mail.rakuten.com>
2014-11-12 23:49:20 +0900

    Moved nginx attribute

--- attributes/nginx.rb
+++ attributes/nginx.rb
@@ -1,1 +1,4 @@
-default['apt-mirror']['nginx']['port']          = 80
+default['apt-mirror']['nginx']['port']    = 80
+default['apt-mirror']['nginx']['aliases'] = {
+  'trusty' => 'gb.archive.ubuntu.com'
+}
